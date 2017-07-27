89a5c4f4b5470a70d8959c303819c704641b352f
Luis Paredes <lparedesl@gmail.com>
2017-07-27 14:15:39 -0400

    deleted env.js in config folder. Added it to .gitignore

--- config/env.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	DATABASE_NAME: "heroku_3fa10fb1471fdc3",
-	DATABASE_USERNAME: "bac5bed9178115",
-	DATABASE_PASSWORD: "b761fb10",
-	DATABASE_HOST: "us-cdbr-iron-east-03.cleardb.net",
-	DATABASE_DIALECT: "mysql"
-};
