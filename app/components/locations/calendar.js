bbd7dcf6137b2c9e3bcabb66808806867e3f2044
Luis Paredes <lparedesl@gmail.com>
2017-08-05 08:22:06 -0400

    added action to Calendar component to get reservations from db and display them on calendar

--- app/components/locations/calendar.js
+++ app/components/locations/calendar.js
@@ -1,48 +1,113 @@
-import React, { Component } from "react";
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import $ from 'jquery';
+import fullcalendar from 'fullcalendar';
+import {connect} from 'react-redux';
+import {getFieldReservations} from '../../actions';
 
 class Calendar extends Component {
-  render() {
-    return (
-      <div>
-        <div className="m-heading-1 border-green m-bordered">
-          <div className="form-group">
+    constructor() {
+        super();
+
+        this.onFieldSelect = this.onFieldSelect.bind(this);
+        this.renderOptGroups = this.renderOptGroups.bind(this);
+    }
+
+    componentDidMount() {
+        this.props.getFieldReservations(this.props.field);
+    }
+
+    renderOptGroups() {
+        const {location} = this.props;
+
+        return _.map(location.sports, sport => {
+            return (
+                <optgroup label={sport.name} key={sport.name}>
+                    {this.renderOptions(sport, location.id)}
+                </optgroup>
+            )
+        })
+    }
+
+    renderOptions(sport, id) {
+        let fields = _.filter(sport.fields, data => { return data.location_id === id; });
+
+        return _.map(fields, field => {
+            return <option value={field.field_number} key={field.id}>Field {field.field_number}</option>
+        })
+    }
+
+    renderCalendar() {
+        const {fieldReservations} = this.props;
+
+        if (!fieldReservations) {
+            return <div>Loading...</div>;
+        }
+
+        $("#field-schedule").fullCalendar('destroy');
+        $("#field-schedule").fullCalendar({
+            height: 500,
+            header: {},
+            defaultView: 'agendaWeek',
+            slotMinutes: 15,
+            editable: true,
+            businessHours: {
+                dow: [ 1, 2, 3, 4, 5 ],
+                start: fieldReservations.openTime,
+                end: fieldReservations.closeTime,
+            },
+            events: fieldReservations.reservations
+        });
+    }
+
+    onFieldSelect(option) {
+        this.props.onFieldSelect(option);
+        this.props.getFieldReservations(option);
+        this.renderCalendar();
+    }
+
+    render() {
+        return (
             <div className="row">
-              <label className="control-label col-md-6">Field Select</label>
-              <div className="col-md-6">
-                <select className="bs-select form-control">
-                  <optgroup label="Football">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                  </optgroup>
-                  <optgroup label="Tennis">
-                    <option>1</option>
-                    <option>2</option>
-                  </optgroup>
-                </select>
+              <div className="col-md-12">
+                <Link className="btn blue btn-outline reserve-btn" to="/locations/reserve-field">Reserve Field</Link>
+                <div className="m-heading-1 border-green m-bordered">
+                  <div className="form-group">
+                    <div className="row">
+                      <label className="control-label col-md-6">Select Field</label>
+                      <div className="col-md-6">
+                        <select className="bs-select form-control" onChange={event => this.onFieldSelect(event.target.value)}>
+                          <option value=""></option>
+                            {this.renderOptGroups()}
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="portlet light portlet-fit bordered calendar">
+                  <div className="portlet-title">
+                    <div className="caption">
+                      <i className=" icon-layers font-green"></i>
+                      <span className="caption-subject font-green bold uppercase">Field Schedule</span>
+                    </div>
+                  </div>
+                  <div className="portlet-body">
+                    <div className="row">
+                      <div className="col-sm-12">
+                        <div id="field-schedule" className="has-toolbar"> </div>
+                          {this.renderCalendar()}
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-        <div className="portlet light portlet-fit bordered calendar">
-          <div className="portlet-title">
-            <div className="caption">
-              <i className=" icon-layers font-green"></i>
-              <span className="caption-subject font-green sbold uppercase">Calendar</span>
-            </div>
-          </div>
-          <div className="portlet-body">
-            <div className="row">
-              <div className="col-md-9 col-sm-12">
-                <div id="calendar" className="has-toolbar"> </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+        )
+    }
 }
 
-// Exporting this component as the default (only) export
-export default Calendar;
+function mapStateToProps(state) {
+    return {fieldReservations: state.fieldReservations}
+}
+
+export default connect(mapStateToProps, {getFieldReservations})(Calendar);
