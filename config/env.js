d50e5b538e31966c96d4581581f6369fa4d6afb9
Luis Paredes <lparedesl@gmail.com>
2017-08-10 10:58:12 -0400

    added env.js and modified .gitignore

---
+++ config/env.js
@@ -0,0 +1,8 @@
+module.exports = {
+    DATABASE_NAME:                  "heroku_3fa10fb1471fdc3",
+    DATABASE_USERNAME:              "bac5bed9178115",
+    DATABASE_PASSWORD:              "b761fb10",
+    DATABASE_HOST:                  "us-cdbr-iron-east-03.cleardb.net",
+    DATABASE_DIALECT:               "mysql",
+    GEOCODING_KEY:                  "AIzaSyCfBFHpV5XvLc9BLKTLrmMlAMD3CupljKA"
+};
